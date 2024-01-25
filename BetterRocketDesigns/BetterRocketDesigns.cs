ecfeaaf00e22829c61edc8757eece028e71c939c
José Cabo <josecabocarsi@gmail.com>
2024-01-25 18:10:46 +0100

    Revert "10 add nightlycanary version"

--- BetterRocketDesigns/BetterRocketDesigns.cs
+++ BetterRocketDesigns/BetterRocketDesigns.cs
@@ -17,8 +17,6 @@ namespace BetterRocketDesigns
     [KSPAddon(KSPAddon.Startup.EditorAny, false)]
     public class BetterRocketDesigns : MonoBehaviour
     {
-        public const string VERSION = "1.0.0";
-
         private RocketDesignManager _rocketDesignManager;
         private ApplicationLauncherButton _toolbarButton;
         private Part detachedPart;
@@ -30,11 +28,6 @@ namespace BetterRocketDesigns
         private Texture2D loadToolbarButtonIcon;
         private Texture2D saveAsToolbarButtonIcon;
         private ToolbarButtonMode _toolbarButtonMode;
-
-        private void Awake()
-        {
-            Debug.Log("BetterRocketDesigns version: " + VERSION);
-        }
 
         private void Start()
         {
